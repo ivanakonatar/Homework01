e2327a5c165ffffffd9d6f917c2f032edbaf795e
Ivana Konatar <ivana.konatar@udg.edu.me>
2018-02-27 23:46:26 +0100

    Fourth exercise

---
+++ ex04.py
@@ -0,0 +1,10 @@
+# File : chaos.py
+# A simple program illustrating chaotic behavios.
+
+def main():
+    print("This program illustrades a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(20):
+        x = 3.9 * x * (1 - x)
+        print(x)
+main()
